dd75935781a7199db1fb1da0e2d9e6d25571421c
KirbyClaro <kirbychristianclaro@gmail.com>
2025-06-18 12:09:11 +0800

    add a while function that starts the battle that starts the match where the first move is the player

--- battle.py
+++ battle.py
@@ -3,3 +3,19 @@ from player import Player
 from enemy import Enemy
 
 def battle(player: Player, opponent: Enemy) -> None:
+    
+    print("\n===== BATTLE START =====\n")
+    
+    while player.is_alive() and opponent.is_alive():
+        # status display
+        player.status()
+        opponent.status()
+        print()
+    
+    match player.choose_action():
+            case "1":
+                player.attack(opponent)
+            case "2":
+                player.heal(20)
+            case _:
+                print("Invalid choice — you hesitate and lose the turn.")
