c36ecae3e63d12c3aea378d9db51fad42f997214
KirbyClaro <kirbychristianclaro@gmail.com>
2025-06-18 12:13:10 +0800

    added def main to run the program

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from player import Player
 from enemy import Enemy
 from battle import battle
+
+def main() -> None:
+    hero   = Player("Hero",   health=100, attack_power=20, defense=5)
+    goblin = Enemy ("Goblin", health=80,  attack_power=15, defense=3)
+    battle(hero, goblin)
+
+if __name__ == "__main__":
+    main()
